1e723deddbb8754febb9f59ffcd17b555ba30870
Radomir Ribic <45924499+richierr@users.noreply.github.com>
2020-10-28 00:26:50 +0100

    third commit

--- app/src/main/java/com/example/bazaglumaca/MainActivity.java
+++ app/src/main/java/com/example/bazaglumaca/MainActivity.java
@@ -3,29 +3,49 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.widget.Toolbar;
 import androidx.appcompat.app.AppCompatActivity;
-
-
+import androidx.drawerlayout.widget.DrawerLayout;
 
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
+import com.melegy.redscreenofdeath.RedScreenOfDeath;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity  {
     private Toolbar toolbar;
+    private List<String> drawerList=new ArrayList<>();
+    private NavigationView drawerLayout;
+
+
+    private void addDrawerLayout(){
+        drawerLayout=findViewById(R.id.my_drawer);
+        ListView listView=findViewById(R.id.my_listview_dr);
+        drawerList.add("Actors");
+        drawerList.add("Settings");
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,R.layout.my_list_item,drawerList);
+        listView.setAdapter(adapter);
+    }
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        RedScreenOfDeath.init(getApplication());
         setContentView(R.layout.activity_main);
         Toolbar toolbar=findViewById(R.id.my_toolbar);
 
         setSupportActionBar(toolbar);
-
+        addDrawerLayout();
     }
 
     @Override
